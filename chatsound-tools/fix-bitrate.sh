a9483089297c2cb4dd1bd41fe42f463a8886c4a8
Ayane Satomi <ayane@vignetteapp.org>
2022-04-26 18:37:33 +0800

    Add chatsound shit I use
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

---
+++ chatsound-tools/fix-bitrate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# You can convert this to also convert any MP3 to OGG
+# But this is intended mostly to fix OGGs with non-compliant bitrates
+
+for file in *.ogg
+  do ffmpeg -i "${file}" -af "aformat=sample_fmts=s16:sample_rates=44100" -acodec libvorbis "conv/${file%.ogg}.ogg"
+done
