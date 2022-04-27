80f1ce4bed3f981ddae4698bc92c9e06a42cb94c
Ayane Satomi <ayane@vignetteapp.org>
2022-04-27 19:40:43 +0800

    Refactor hook to appease PAC3
    Signed-off-by: Ayane Satomi <ayane@vignetteapp.org>

--- afk_hook.lua
+++ afk_hook.lua
@@ -1,12 +1,20 @@
 -- This should be added either as a autorun
 -- or on your own outfit.
+hook.Add("AFK", "pac_afk", function(pl, afk)
+    if pl and pl == LocalPlayer() and afk then
+        LocalPlayer():ConCommand("pac_event afk 1")
+    else
+        LocalPlayer():ConCommand("pac_event afk 0")
+    end
+end)
 
-hook.Add("AFK", "pacafk", function(ply, afk)
-    if ply and ply == LocalPlayer() then
-        if afk then
-            LocalPlayer():ConCommand("pac_event afk 1")
-        else
-            LocalPlayer():ConCommand("pac_event afk 0")
-        end
+local away = false hook.Add("Think", "pac_away", function()
+    if not away and not system.HasFocus() then
+        away = true
+        LocalPlayer():ConCommand("pac_event away 1")
+    end
+    if away and system.HasFocus() then
+        away = false
+        LocalPlayer():ConCommand("pac_event away 0")
     end
 end)
