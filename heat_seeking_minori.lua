356f497bc395e4143aef2ccde0f797b88047b508
Ayane Satomi <ayane@vignetteapp.org>
2022-05-15 01:04:04 +0800

    Make the follow script smarter

--- heat_seeking_minori.lua
+++ heat_seeking_minori.lua
@@ -1,10 +1,10 @@
 -- Code by Henke and Minori
 -- to remove the hook please run
--- hook.Remove("StartCommand", "autopilot")
+-- hook.Remove("StartCommand", "heatseekingminori")
 
-local target = eno
+local target = razz
 
-say("[luadev/Autopilot] Now following " .. tostring(target))
+print("[luadev/Autopilot] Now following " .. tostring(target))
 
 
 hook.Add("StartCommand", "heatseekingminori", function(ply, cmd)
@@ -15,13 +15,27 @@ hook.Add("StartCommand", "heatseekingminori", function(ply, cmd)
 	end
 
 
-	-- we wanna run if our target is greater than 512 units
+	-- we wanna run if our target is greater than 256 units
 	-- that way we can still catch up
-	if target:GetPos():Distance(ply:GetPos()) > 512 then
+	if target:GetPos():Distance(ply:GetPos()) > 256 then
 		cmd:AddKey(IN_SPEED)
 		cmd:SetForwardMove(ply:GetRunSpeed())
 	else
 	    cmd:SetForwardMove(ply:GetWalkSpeed())
+	end
+
+	-- if father than 512 units away, noclip
+	if target:GetPos():Distance(ply:GetPos()) > 512 then
+		ply:ConCommand("noclip")
+		cmd:AddKey(IN_SPEED)
+		cmd:SetForwardMove(ply:GetRunSpeed())
+	else
+	    cmd:SetForwardMove(ply:GetWalkSpeed())
+	end
+
+	-- This is too far now, we need to TP at this rate.
+	if target:GetPos():Distance(ply:GetPos()) > 2048 then
+		RunConsoleCommand("aowl", "tp ", target:Name())
 	end
 
 	local ang = cmd:GetViewAngles()
