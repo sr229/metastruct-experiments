d393f291143ae526f12367d00cf318a5f19710df
Ayane Satomi <ayane@vignetteapp.org>
2022-05-15 10:27:44 +0800

    Add nil checks

--- starfallex/starfall_tts_gimpy.lua
+++ starfallex/starfall_tts_gimpy.lua
@@ -9,17 +9,20 @@ hook.add("playerchat", "henkey", function(ply, txt)
     if ply ~= owner() then return end
 
     if string.sub(txt, 1, 1) ~= ";" then return end
-    local arg = string.sub(txt, 2)
+    txt = string.sub(txt, 2)
 
-    if arg ~= nil then return end
 
-    bass.loadURL("https://translate.google.com/translate_tts?ie=UTF-8&q=" .. http.urlEncode(arg) .. "&tl=" .. lang .. "&client=tw-ob", "3d",
+    bass.loadURL("https://translate.google.com/translate_tts?ie=UTF-8&q=" .. http.urlEncode(txt) .. "&tl=" .. lang .. "&client=tw-ob", "3d",
     function(a, err, name)
         -- we dispose the current reference, then we create a new one
         if soundref then soundref:stop() end
+        -- make sure we do not assign nil
+        if not a then return end
         soundref = a
 
         hook.add("think", "soundFollow", function() a:setPos(owner():getPos()) end)
+        -- do not make it play anything if its nil
+        if not soundref then return end
         soundref:play()
     end)
 end)
