f7ed75813053ef062ab7bc59ca9d161220570af2
Ayane Satomi <ayane@vignetteapp.org>
2022-06-22 19:04:44 +0800

    shitty json parse testing thingy lol

---
+++ jsonparse_test.lua
@@ -0,0 +1,14 @@
+local url = "https://raw.githubusercontent.com/sr229/metastruct-experiments/master/starfall_metadata/allowed_google_voices.json"
+local data
+
+http.Fetch(url, function(body, length, headers, code)
+    if length == 0 then
+        print("Invalid length! Length should be non-zero.")
+
+        return
+    end
+
+    data = util.JSONToTable(body)
+    print("DEBUG: " .. tostring(data))
+    PrintTable(data)
+end)
