be360f578ff2167074731cd7f6f0746553f87b9e
Ayase Minori <chinodesuuu@gmail.com>
2024-08-31 02:34:16 +0800

    Move CTP override to prejoin hooks

--- prejoin_hooks/init.lua
+++ prejoin_hooks/init.lua
@@ -1,3 +1,4 @@
 include("metastruct_specific/modules/pickup_hook.lua")
 include("metastruct_specific/modules/use_hook.lua")
 include("metastruct_specific/modules/hook_killer.lua")
+include("metastruct_specific/modules/remove_ctp_playerstep.lua")
--- remove_playerstepsoundtime_ctp.g.lua
+++
@@ -1,3 +0,0 @@
--- This gets rid of the awful PlayerStepSound calculatuion CTP does
--- Which isn't pretty anyway
-function ctp:PlayerStepSoundTime() end
