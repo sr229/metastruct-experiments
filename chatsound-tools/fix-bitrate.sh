60bb0f8d1db9c89752f3340c2eec84f1876a7ead
Ayase Minori <chinodesuuu@gmail.com>
2024-09-20 01:33:13 +0800

    Make script automatically add a conv dir

--- chatsound-tools/fix-bitrate.sh
+++ chatsound-tools/fix-bitrate.sh
@@ -2,6 +2,10 @@
 # You can convert this to also convert any MP3 to OGG
 # But this is intended mostly to fix OGGs with non-compliant bitrates
 
+if [ ! -d "conv/"  ] then
+  mkdir conv
+fi
+
 for file in *.ogg
   do ffmpeg -i "${file}" -af "aformat=sample_fmts=s16:sample_rates=44100" -acodec libvorbis "conv/${file%.ogg}.ogg"
 done
